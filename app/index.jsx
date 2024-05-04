addd4663de4901230529679c8a399f42f0f61d5d
tulsyanshubham <141614975+tulsyanshubham@users.noreply.github.com>
2024-05-04 16:02:34 +0530

    upload

---
+++ app/index.jsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+
+const index = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">Aora!</Text>
+      <StatusBar style='auto' />
+      <Link href={'./profile'} >Go to Profile</Link>
+    </View>
+  )
+}
+
+export default index
